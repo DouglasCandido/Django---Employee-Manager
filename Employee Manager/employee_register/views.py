81e1191ce4f171ec5c748db1e5c33c45e4235a6f
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-01 13:27:45 -0300

    A funcionalidade Atualizar Empregado foi melhorada.

--- Employee Manager/employee_register/views.py
+++ Employee Manager/employee_register/views.py
@@ -8,13 +8,21 @@ def employee_list(request):
     context = {'employee_list':Employee.objects.all()}
     return render(request, "employee_register/employee_list.html", context)
 
-def employee_form(request):
+def employee_form(request, id = 0):
     if request.method == "GET":
-        form = EmployeeForm()
+        if id == 0:  # Se o id passado for 0 (Default), então exibirá um formulário em branco para ser utilizado em uma operação de insert
+            form = EmployeeForm()
+        else: # Se o id passado for diferente de 0, exibirá um formulário preenchido com os dados do Empregado correspondentes à chave primária referente ao id
+            employee = Employee.objects.get(pk = id)
+            form = EmployeeForm(instance = employee)
         return render(request, "employee_register/employee_form.html", {'form': form})
     else:
-        form = EmployeeForm(request.POST)
-        if form.is_valid():
+        if id == 0: # Operação de inserir um novo empregado
+            form = EmployeeForm(request.POST)
+        else: # Operação de atualizar um empregado já existente com a chave primária referente ao id com os dados passados pelo formulário na requisição POST
+            employee = Employee.objects.get(pk = id)
+            form = EmployeeForm(request.POST, instance = employee)
+        if form.is_valid(): 
             form.save()
         return redirect('/employee/list')            
 
